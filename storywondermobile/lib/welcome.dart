03b02dce11fd503909d032f820f02822995ab618
skasmi <skasmi@FRATELLO>
2024-02-20 01:39:29 +0100

    update button

--- storywondermobile/lib/welcome.dart
+++ storywondermobile/lib/welcome.dart
@@ -9,7 +9,8 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF043743), // Set the background color to black
+      backgroundColor:
+          const Color(0xFF043743), // Set the background color to black
       body: SafeArea(
         child: Center(
           // Wrap the Column with Center
@@ -19,17 +20,17 @@ class WelcomePage extends StatelessWidget {
             children: [
               Column(
                 children: [
-                  SizedBox(
+                  const SizedBox(
                     height: 160,
                   ),
                   Image.asset(
                     'assets/images/logo_story_wonder.png',
                     width: 177,
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 10,
                   ),
-                  Text(
+                  const Text(
                     'let\'s find the magic',
                     style: TextStyle(
                       color: Color.fromARGB(255, 255, 255, 255),
@@ -41,8 +42,8 @@ class WelcomePage extends StatelessWidget {
                 ],
               ),
               Padding(
-                padding:
-                    EdgeInsets.only(bottom: 60), // Set top padding to 60 pixels
+                padding: const EdgeInsets.only(
+                    bottom: 60), // Set top padding to 60 pixels
                 child: SizedBox(
                   width: 200, // Adjust the width as needed
                   height: 50, // Adjust the height as needed
@@ -56,10 +57,10 @@ class WelcomePage extends StatelessWidget {
                       ),
                       backgroundColor: Color(0xFF1F72C0),
                     ),
-                    child: Text(
+                    child: const Text(
                       'Get Started',
                       style: TextStyle(
-                        fontFamily: 'myfont',
+                        fontFamily: 'Schyler',
                         fontSize: 25,
                         color: Color.fromARGB(255, 255, 255, 255),
                       ),
